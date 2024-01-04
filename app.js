32807d3779b4d5e95e7a220570a9f76327917efa
unknown <dejanmaric259@gmail.com>
2024-01-04 19:19:56 +0100

    added ejs renders

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 //express app 
 const app = express();
 
+//register view engine
+
+app.set('view engine', 'ejs')
 
 //listen for req
 app.listen(3000); 
@@ -10,11 +13,11 @@ app.listen(3000);
 
 app.get('/', (req,res)=>{
     // res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html', {root: __dirname})
+    res.render('index');
 })
 app.get('/about', (req,res)=>{
     // res.send('<p>About Page</p>')
-    res.sendFile('./views/about.html', {root: __dirname})
+   res.render('about')
 })
 
 
@@ -26,5 +29,5 @@ app.get('/about-us', (req,res)=>{
 // 404 page
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html', {root:__dirname})
+    res.status(404).render('404')
 })
