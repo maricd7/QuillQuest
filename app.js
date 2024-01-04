cf2361ff738fb9311d027d94a5f53c771b59a1e0
unknown <dejanmaric259@gmail.com>
2024-01-04 19:50:59 +0100

    adding partials and styling

--- app.js
+++ app.js
@@ -10,24 +10,28 @@ app.set('view engine', 'ejs')
 //listen for req
 app.listen(3000); 
 
-
+const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
 app.get('/', (req,res)=>{
-    // res.send('<p>Home page</p>')
-    res.render('index');
+    res.render('index', {title:'Home', blogs});
 })
 app.get('/about', (req,res)=>{
-    // res.send('<p>About Page</p>')
-   res.render('about')
+   res.render('about',  {title:'About'})
 })
-
-
 //redirects
 app.get('/about-us', (req,res)=>{
     res.redirect('/about')
 })
 
-// 404 page
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create',  {title:'Create New Blog'});
+  });
+
+// 404 page
 app.use((req,res)=>{
-    res.status(404).render('404')
+    res.status(404).render('404',  {title:'404'})
 })
